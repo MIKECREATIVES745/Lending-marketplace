32a1308563722277778f0478cf1ebdbcdd87ce0d
Mike Mwenya <mikecreatives745@gmail.com>
2026-05-26 22:58:39 +0200

    updates API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://smart-money-api.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://smartmoney-h0wa.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
